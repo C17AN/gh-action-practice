1722b7c7c802531e992161ef7e4a0555d0b9b7c0
c17an <kimchanmin1@gmail.com>
2021-07-11 21:05:53 +0900

    feat:  PR 제출시 코멘트 달리는 기능 추가

--- src/action.js
+++ src/action.js
@@ -1,5 +1,17 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 async function run() {
-  console.log("Hello, world");
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for sumbmitting a pull request!",
+  });
 }
 
 run();
